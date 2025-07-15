e059466be8fa13bf467fb19ad7704ef3442ed629
varshavangasam <138878507+varshavangasam@users.noreply.github.com>
2025-07-15 20:20:56 +0530

    Update and rename .env to test_reddit.py

---
+++ test_reddit.py
@@ -0,0 +1,14 @@
+import praw
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+reddit = praw.Reddit(
+    client_id=os.getenv("REDDIT_CLIENT_ID"),
+    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+    user_agent=os.getenv("REDDIT_USER_AGENT"),
+)
+
+print("✅ Reddit instance created")
+print("Authenticated as:", reddit.user.me())
